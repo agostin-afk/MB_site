1bb901dfe9c3cc10ca709c8ad2da10f80e66c13d
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-07-24 17:41:17 -0300

    limpeza

--- app/backend/scripts/migrate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo 'Executando o makemigrations e migrate'
-
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
--- app/backend/scripts/runserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo 'Executando o runserver'
-
-python manage.py runserver 0.0.0.0:8000
